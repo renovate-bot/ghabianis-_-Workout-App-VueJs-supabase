f1d18ffc86980a1441d050f096a517c020db1ede
ghabianis <anisghabi8@gmail.com>
2022-06-21 18:50:38 +0200

    workoutfirstcommit

---
+++ src/router/index.js
@@ -0,0 +1,47 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Login from '../views/Login.vue';
+import Register from '../views/Register.vue';
+import Create from '../views/Create.vue';
+import ViewWorkout from '../views/ViewWorkout.vue';
+
+
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+  },
+  {
+    path: "/create",
+    name: "Create",
+    component: Create,
+  },
+  {
+    path: "/view-workout/:workoutId",
+    name: "View-Workout",
+    component: ViewWorkout,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+// Change document titles
+
+// Route guard for auth routes
+
+export default router;
